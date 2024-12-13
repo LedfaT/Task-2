ad9a60e703d9f37d226b5242b5d53466ea3d045c
David Zaitsev <zaitsevdavid4@gmail.com>
2024-12-13 23:34:19 +0200

    add date, time and number of pages

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
 "use strict";
+
+const images = document.querySelectorAll("img");
+const infomation = document.querySelector(".information");
+const now = new Date();
+
+const printCurrentTime = function () {
+  infomation.innerHTML = "";
+
+  const optionsDate = {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+  };
+  const optionsTime = {
+    hour: "numeric",
+    minute: "numeric",
+  };
+
+  infomation.insertAdjacentHTML(
+    "beforeend",
+    `<p>number of images is ${images.length}</p>
+      <p>${new Intl.DateTimeFormat(navigator.language, optionsDate).format(
+        now
+      )}</p>
+      
+      <p>${new Intl.DateTimeFormat(navigator.language, optionsTime).format(
+        now
+      )}</p>`
+  );
+};
+
+printCurrentTime();
